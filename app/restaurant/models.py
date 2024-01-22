1b6b67343b0e3ed9a3ea02ebff4314c63afa59fb
alex-pythonista <indrajit.alex@gmail.com>
2024-01-22 20:41:21 +0600

    user and restaurant app models

--- app/restaurant/models.py
+++ app/restaurant/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+from user.models import OwnerProfile, EmployeeProfile
+
+
+class Restaurant(models.Model):
+    owner = models.ForeignKey(OwnerProfile, on_delete=models.CASCADE)
+    restaurant_name = models.CharField(max_length=200)
+    address = models.TextField(max_length=500)
+    hotline_number = models.CharField(max_length=15)
+    opening_hours = models.TextField(max_length=1000)
+    managers = models.ManyToManyField(EmployeeProfile, related_name="restaurant_managers")
+
+    def __str__(self) -> str:
+        return self.restaurant_name
+    
+
+class Menu(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.restaurant.restaurant_name} - {self.name}"
+    
+
+class Item(models.Model):
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    item_name = models.CharField(max_length=200)
+    description = models.TextField(max_length=500)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self) -> str:
+        return f"{self.item_name}-{self.menu.name}"
--- app/user/models.py
+++ app/user/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+# using the default user model
+User = get_user_model()
+
+
+class OwnerProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    license_no = models.IntegerField(blank=True, null=True)
+    passport_no = models.IntegerField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.user.username
+    
+
+class EmployeeProfile(models.Model):
+    SHIFT = (
+        ("MORNING", "MORNING"),
+        ("EVENING", "EVENING"),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    employee_id = models.IntegerField(blank=True, null=True)
+    date_joined = models.DateField(blank=True, null=True)
+    shift = models.CharField(max_length=50, choices=SHIFT, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.user.username}-{self.shift}"
